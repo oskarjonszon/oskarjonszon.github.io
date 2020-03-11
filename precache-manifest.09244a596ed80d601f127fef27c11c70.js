3e75ac9c9052f3dfef1a19d90331b04dee0c8c7f
Oskar Jönsson <37797439+oskarjonszon@users.noreply.github.com>
2020-03-11 17:58:25 +0100

    Updates

---
+++ precache-manifest.09244a596ed80d601f127fef27c11c70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af966b56677376956ac2f4a46723d25a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a1f2b0b5c9ce6168ec6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b371b344d74bd35d5111",
+    "url": "/static/js/2.13a503ec.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.13a503ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1f2b0b5c9ce6168ec6",
+    "url": "/static/js/main.9b7630d4.chunk.js"
+  },
+  {
+    "revision": "a9fb7b9cfdb9f77a7f5e",
+    "url": "/static/js/runtime-main.d095189c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
